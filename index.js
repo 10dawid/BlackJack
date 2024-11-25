24e58c8edb86bd68a4d9038a3321153bae8fe9d5
Dawid Łęczycki <leczyckidawid13@gmail.com>
2024-11-25 20:34:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstCard = 12;
+let secondCard = 9;
+
+let sum = firstCard + secondCard;
+console.log(sum);
+
+if (sum < 21){
+    console.log("Do you want to draw a new card?")
+}
+else if (sum == 21){
+    console.log("u just fuckin won")
+}
