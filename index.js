7500b52ee91d0a214d568dbfd35960d2cc0a9299
Dawid Łęczycki <leczyckidawid13@gmail.com>
2024-11-25 23:51:19 +0100

    added basic stuff
    start btn
    new card btn

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-let firstCard = 12;
-let secondCard = 9;
+let messageEL = document.querySelector("#message-el")
+let sumEL = document.querySelector("#sum-el")
+let cardsEL = document.querySelector("#cards-el")
+let firstCard
+let secondCard
+let hasBlackJack = false
+let isAlive = true
+let hasStarted = false
+let message = ""
+let sum
 
-let sum = firstCard + secondCard;
-console.log(sum);
 
-if (sum < 21){
-    console.log("Do you want to draw a new card?")
+
+function startGame(){
+    hasStarted = true
+    firstCard = 10
+    secondCard = 11
+    cardsEL.textContent = "Cards: " + firstCard + " : " + secondCard
+    
+    sum = firstCard + secondCard
+    if (sum < 21){
+        message= "Do you want to draw a new card?"
+    }
+    else if (sum === 21){
+        message = "Congrats!! You've got Blackjack!"
+        hasBlackJack = true
+    }
+    else{
+        message = "you are out of the game!"
+        isAlive = false
+    }
+    messageEL.textContent = message
+    sumEL.textContent = "Sum: " + sum
 }
-else if (sum == 21){
-    console.log("u just fuckin won")
+function newCard(){
+    if (hasStarted){
+        let nextCard = 10
+        sum += nextCard
+        sumEL.textContent = "Sum: " + sum
+        cardsEL.textContent += " : " + nextCard
+    }
 }
+console.log(newCard())
